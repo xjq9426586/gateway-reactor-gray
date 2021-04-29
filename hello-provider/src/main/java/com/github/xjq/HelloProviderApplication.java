57d9f58d85311a3cab6ed77b40dd9dbae0d7e458
xjq <931408673@qq.com>
2021-04-29 11:05:51 +0800

    s

--- hello-provider/src/main/java/com/github/xjq/HelloProviderApplication.java
+++ hello-provider/src/main/java/com/github/xjq/HelloProviderApplication.java
@@ -3,10 +3,18 @@ package com.github.xjq;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class HelloProviderApplication {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloProviderApplication.class, args);
