4636aa54717c95c9f5e73412612d9f5b3c25165a
xjq <931408673@qq.com>
2021-04-29 14:02:06 +0800

    s

--- hello-provider/src/main/java/com/github/xjq/helloprovider/ProviderImpl.java
+++ hello-provider/src/main/java/com/github/xjq/helloprovider/ProviderImpl.java
@@ -1,6 +1,5 @@
 package com.github.xjq.helloprovider;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
